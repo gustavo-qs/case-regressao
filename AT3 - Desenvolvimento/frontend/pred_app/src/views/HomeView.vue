f1d6c71b338603063fd729de986fc682d976bbea
Gustavo Queiroz <compgustavo@edu.unifil.br>
2025-05-03 13:33:46 -0300

    :sparkles: starting frontend, users crud complete

---
+++ AT3 - Desenvolvimento/frontend/pred_app/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const changeView = (path) => {
+  router.push(path);
+};
+</script>
+
+<template>
+  <div>
+    <nav>
+      <button @click="changeView('/users')">Gerenciar Usuário</button>
+      <button @click="changeView('/predict')">Prever Novos Dados</button>
+      <button @click="changeView('/history')">Consultar Histórico</button>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
